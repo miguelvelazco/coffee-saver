1763189c2073265004d69b4a3f2b0ae2aa33b39e
ligma <miguel@ninja.services>
2019-12-01 20:48:17 -0500

    upload

--- coffeesaver-smb-enum.sh
+++ coffeesaver-smb-enum.sh
@@ -1,4 +1,8 @@
 echo -e "\033[0m\n*GODZILLA*\n\033[0;32mGGggGGggGG\nooOOooOOoo\nDDddDDddDD\nzzZZzzZZzz\nIIiiIIiiII\nllLLllLLll\nLLllLLllLL\naaAAaaAAaa\033[0m\n*GODZILLA*"
+
+echo -e "creating scans folder"
+mkdir -p scans/
+
 nmap -sP $1 -oG $2.txt
 cat $2.txt | grep "Up" | cut -d " " -f2 > livehost.txt
 
@@ -6,5 +10,5 @@ for i in $(cat livehost.txt);
 do
   echo -e "running smb-enum on $i"
   going="${i}-smb-enum"
-    nmap --script smb-enum* -vvv -p 445,139 $i > $going.txt
+    nmap --script smb-enum* -vvv -p 445,139 $i > scans/$going.txt
 done
--- coffeesaver-smb.sh
+++ coffeesaver-smb.sh
@@ -1,4 +1,8 @@
 echo -e "\033[0m\n*GODZILLA*\n\033[0;32mGGggGGggGG\nooOOooOOoo\nDDddDDddDD\nzzZZzzZZzz\nIIiiIIiiII\nllLLllLLll\nLLllLLllLL\naaAAaaAAaa\033[0m\n*GODZILLA*"
+
+echo -e "creating scans folder"
+mkdir -p scans/
+
 nmap -sP $1 -oG $2.txt
 cat $2.txt | grep "Up" | cut -d " " -f2 > livehost.txt
 
@@ -6,5 +10,5 @@ for i in $(cat livehost.txt);
 do
   echo -e "running ms smb check on $i"
   smbchecker="${i}-smb-ms-check"
-  nmap --script smb-vuln-ms* -vvv $i | tee $smbchecker.txt
+  nmap --script smb-vuln-ms* -vvv $i | tee scans/$smbchecker.txt
 done
